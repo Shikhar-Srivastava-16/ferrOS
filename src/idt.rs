ec5e22031d2c79d7d8e910cefee70921abe18b6c
Shikhar Srivastava <shikhars2006@gmail.com>
2026-01-08 09:15:19 +0000

    working with typing registered

--- src/idt.rs
+++ src/idt.rs
@@ -23,26 +23,38 @@ lazy_static! {
 
         idt.divide_error.set_handler_fn(divide_error_handler);
         idt.debug.set_handler_fn(debug_handler);
-        idt.non_maskable_interrupt.set_handler_fn(non_maskable_interrupt_handler);
+        idt.non_maskable_interrupt
+            .set_handler_fn(non_maskable_interrupt_handler);
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.overflow.set_handler_fn(overflow_handler);
-        idt.bound_range_exceeded.set_handler_fn(bound_range_exceeded_handler);
+        idt.bound_range_exceeded
+            .set_handler_fn(bound_range_exceeded_handler);
         idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
-        idt.device_not_available.set_handler_fn(device_not_available_handler);
+        idt.device_not_available
+            .set_handler_fn(device_not_available_handler);
         idt.invalid_tss.set_handler_fn(invalid_tss_handler);
-        idt.segment_not_present.set_handler_fn(segment_not_present_handler);
-        idt.stack_segment_fault.set_handler_fn(stack_segment_fault_handler);
-        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
+        idt.segment_not_present
+            .set_handler_fn(segment_not_present_handler);
+        idt.stack_segment_fault
+            .set_handler_fn(stack_segment_fault_handler);
+        idt.general_protection_fault
+            .set_handler_fn(general_protection_fault_handler);
         idt.page_fault.set_handler_fn(page_fault_handler);
-        idt.x87_floating_point.set_handler_fn(x87_floating_point_handler);
+        idt.x87_floating_point
+            .set_handler_fn(x87_floating_point_handler);
         idt.alignment_check.set_handler_fn(alignment_check_handler);
         idt.machine_check.set_handler_fn(machine_check_handler);
-        idt.simd_floating_point.set_handler_fn(simd_floating_point_handler);
+        idt.simd_floating_point
+            .set_handler_fn(simd_floating_point_handler);
         idt.virtualization.set_handler_fn(virtualization_handler);
-        idt.cp_protection_exception.set_handler_fn(cp_protection_exception_handler);
-        idt.hv_injection_exception.set_handler_fn(hv_injection_exception_handler);
-        idt.vmm_communication_exception.set_handler_fn(vmm_communication_exception_handler);
-        idt.security_exception.set_handler_fn(security_exception_handler);
+        idt.cp_protection_exception
+            .set_handler_fn(cp_protection_exception_handler);
+        idt.hv_injection_exception
+            .set_handler_fn(hv_injection_exception_handler);
+        idt.vmm_communication_exception
+            .set_handler_fn(vmm_comms_exception_handler);
+        idt.security_exception
+            .set_handler_fn(security_exception_handler);
 
         idt[Interrupts::Timer as u8].set_handler_fn(timer_interrupt_handler);
         idt[Interrupts::Keyboard as u8].set_handler_fn(keyboard_int_handler);
@@ -55,67 +67,91 @@ pub fn init_idt() {
     IDT.load();
 }
 
+#[allow(unused)]
 extern "x86-interrupt" fn divide_error_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  divide_error_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn debug_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  debug_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn non_maskable_interrupt_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  non_maskable_interrupt_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn overflow_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  overflow_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn bound_range_exceeded_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  bound_range_exceeded_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn invalid_opcode_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  invalid_opcode\n");
 }
+#[allow(unused)]
 extern "x86-interrupt" fn device_not_available_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  device_not_available_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn invalid_tss_handler(_st_fr: InterruptStackFrame, code: u64) {
     crate::dprintf!("TODO INT:  invalid_tss_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn segment_not_present_handler(_st_fr: InterruptStackFrame, code: u64) {
     crate::dprintf!("TODO INT:  segment_not_present_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn stack_segment_fault_handler(_st_fr: InterruptStackFrame, code: u64) {
     crate::dprintf!("TODO INT:  stack_segment_fault_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn general_protection_fault_handler(_st_fr: InterruptStackFrame, code: u64) {
     crate::dprintf!("TODO INT:  general_protection_fault_handler\n",);
 }
-extern "x86-interrupt" fn page_fault_handler(_st_fr: InterruptStackFrame, code: PageFaultErrorCode) {
+#[allow(unused)]
+extern "x86-interrupt" fn page_fault_handler(
+    _st_fr: InterruptStackFrame,
+    code: PageFaultErrorCode,
+) {
     crate::dprintf!("TODO INT:  page_fault_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn x87_floating_point_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  x87_floating_point_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn alignment_check_handler(_st_fr: InterruptStackFrame, code: u64) {
     crate::dprintf!("TODO INT:  alignment_check_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn machine_check_handler(_st_fr: InterruptStackFrame) -> ! {
     crate::dprintf!("TODO INT:  machine_check_handler\n",);
-    loop{}
+    loop {}
 }
+#[allow(unused)]
 extern "x86-interrupt" fn simd_floating_point_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  simd_floating_point_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn virtualization_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  virtualization_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn cp_protection_exception_handler(_st_fr: InterruptStackFrame, code: u64) {
     crate::dprintf!("TODO INT:  cp_protection_exception_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn hv_injection_exception_handler(_st_fr: InterruptStackFrame) {
     crate::dprintf!("TODO INT:  hv_injection_exception_handler\n",);
 }
-extern "x86-interrupt" fn vmm_communication_exception_handler(_st_fr: InterruptStackFrame, code: u64) {
+#[allow(unused)]
+extern "x86-interrupt" fn vmm_comms_exception_handler(_st_fr: InterruptStackFrame, code: u64) {
     crate::dprintf!("TODO INT:  vmm_communication_exception_handler\n",);
 }
+#[allow(unused)]
 extern "x86-interrupt" fn security_exception_handler(_st_fr: InterruptStackFrame, code: u64) {
     crate::dprintf!("TODO INT:  security_exception_handler\n",);
 }
@@ -155,9 +191,10 @@ extern "x86-interrupt" fn keyboard_int_handler(_stack_frame: InterruptStackFrame
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    // crate::dprintf!(".");
+    crate::dprintf!(".");
 
     unsafe {
         PICS.lock().notify_end_of_interrupt(Interrupts::Timer as u8);
     }
 }
+
